967da8bd49ec3b34b2ab5b09179fa0098269d2c9
Jan Frode Håskjold <jan.frode.haskjold@fremtind.no>
2026-09-26 19:59:38 +0200

    fix: call ts_ibm2iso8601 before conversion

--- src/noxDbUtf8.c
+++ src/noxDbUtf8.c
@@ -3307,17 +3307,15 @@ PNOXNODE  nox_SetTimeStampByNameVC (PNOXNODE pNode, PLVARCHAR  Name, TIMESTAMP V
    }
 
    substr(s , (PUCHAR) &Value  , sizeof(TIMESTAMP));
-   stre2a(s,s);
    if (isoTimestamp == ON) ts_ibm2iso8601(s);
-   return nox_SetValueByName(pNode , plvc2str(Name) , s, NOX_VALUE );
+   return nox_SetValueByName(pNode , plvc2str(Name) , stre2a(s,s), NOX_VALUE );
 }
 PNOXNODE  nox_TimeStamp (TIMESTAMP Value )
 {
    UCHAR  s [32];
    substr(s , (PUCHAR) &Value  , sizeof(TIMESTAMP));
-   stre2a(s,s);
    if (isoTimestamp == ON) ts_ibm2iso8601(s);
-   return NewNode (NULL, s, NOX_VALUE );
+   return NewNode (NULL, stre2a(s,s), NOX_VALUE );
 }
 /* -------------------------------------------------------------
     Set decimal  by name
